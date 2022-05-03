79d6093b463c09272449dfc74b4301c71e8c5c24
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2022-05-03 18:11:28 +0530

    feat: set token in local storage

--- src/context/authContext/authenticationContext.jsx
+++ src/context/authContext/authenticationContext.jsx
@@ -1,8 +1,32 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { userLoginservice } from "../../services";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState("");
+
+  const loginUser = async (email, password) => {
+    try {
+      const response = await userLoginservice(email, password);
+      if (response.status === 200) {
+        localStorage.setItem(
+          "login",
+          JSON.stringify({ token: response.data.encodedToken })
+        );
+        setToken(response.data.encodedToken);
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ user: response.data.foundUser })
+        );
+        setUser(response.data.foundUser);
+      }
+    } catch (error) {
+      console.error("Error in login functionality");
+    }
+  };
+
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
 };
 
