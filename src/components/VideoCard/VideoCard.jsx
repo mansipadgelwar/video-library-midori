7a6435a2f35f7ec7125e8cf8dbc44d0ab8a0cb24
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2022-05-19 13:39:03 +0530

    style: video thumbnail

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,14 +1,18 @@
 import "../VideoCard/VideoCard.css";
 import { Link } from "react-router-dom";
 
-const VideoCard = ({ _id, title, category, url }) => {
+const VideoCard = ({ id, title }) => {
   return (
     <div className="video-card-container">
       <div>
         <Link to="/">
           <span className="material-icons icon">favorite_border</span>
         </Link>
-        <img src="https://picsum.photos/300/170" alt="video" />
+        <img
+          src={`http://i3.ytimg.com/vi/${id}/hqdefault.jpg`}
+          alt={`${title} thumbnail`}
+          className="video-thumbnail img-responsive"
+        />
       </div>
       <div className="video-description">
         <div className="video-heading">{title}</div>
