ba362f28f31aed8aa5cc05c42609b80f33116251
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2022-05-21 11:05:43 +0530

    feat:add playlist form modal

--- src/components/PlaylistForm/PlaylistForm.jsx
+++ src/components/PlaylistForm/PlaylistForm.jsx
@@ -2,31 +2,29 @@ import "../PlaylistForm/PlaylistForm.css";
 
 const PlaylistForm = () => {
   return (
-    <div className="playlist-form">
-      <div className="modal-wrapper">
-        <article className="form-container modal">
-          <button className="modal-close-icon">
-            <span className="material-icons">close</span>
-          </button>
-          <form className="form" action="">
-            <h3 className="h3">Create New Playlist</h3>
-            <label htmlFor="playlist" className="input-label ">
-              Playlist Name
-            </label>
-            <div className="input">
-              <input
-                className="input"
-                type="text"
-                id="playlist"
-                name="playlist"
-                placeholder="My Playlist"
-              />
-            </div>
+    <div className="modal-wrapper">
+      <article className="playlist-form-container modal">
+        <button className="modal-close-icon">
+          <span className="material-icons">close</span>
+        </button>
+        <form className="playlist-form" action="">
+          <h3 className="h3">Create New Playlist</h3>
+          <label htmlFor="playlist" className="input-label ">
+            Playlist Name
+          </label>
+          <div className="input">
+            <input
+              className="input"
+              type="text"
+              id="playlist"
+              name="playlist"
+              placeholder="My Playlist"
+            />
+          </div>
 
-            <button className="btn btn-secondary">Create Playlist</button>
-          </form>
-        </article>
-      </div>
+          <button className="btn btn-secondary">Create Playlist</button>
+        </form>
+      </article>
     </div>
   );
 };
--- src/pages/PlaylistListingPage/PlaylistLisingPage.jsx
+++ src/pages/PlaylistListingPage/PlaylistLisingPage.jsx
@@ -1,27 +1,32 @@
 import "../PlaylistListingPage/PlaylistListingPage.css";
-import { PlaylistCard } from "../../components";
+import { PlaylistCard, PlaylistForm } from "../../components";
 
 const PlaylistListingPage = () => {
   const openModalToCreateNewPlaylist = () => {};
   return (
-    <div className="main-content-page">
-      <div className="menu-bar">
-        <div className="page-title h3 text-bold">My Playlists</div>
-        <div>
-          <button
-            className="btn btn-cta"
-            onClick={openModalToCreateNewPlaylist}
-          >
-            Create New Playlist
-          </button>
+    <div>
+      <PlaylistForm />
+      <div className="modal-background-page">
+        <div className="main-content-page">
+          <div className="menu-bar">
+            <div className="page-title h3 text-bold">My Playlists</div>
+            <div>
+              <button
+                className="btn btn-cta"
+                onClick={openModalToCreateNewPlaylist}
+              >
+                Create New Playlist
+              </button>
+            </div>
+          </div>
+          <div className="history-video-container">
+            <PlaylistCard />
+            <PlaylistCard />
+            <PlaylistCard />
+            <PlaylistCard />
+            <PlaylistCard />
+          </div>
         </div>
-      </div>
-      <div className="history-video-container">
-        <PlaylistCard />
-        <PlaylistCard />
-        <PlaylistCard />
-        <PlaylistCard />
-        <PlaylistCard />
       </div>
     </div>
   );
