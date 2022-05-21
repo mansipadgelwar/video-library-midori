95cab250cc1289d9c9b2190f9d440d463b656a36
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2022-05-21 10:22:44 +0530

    style: add playlist modal component

--- src/components/PlaylistForm/PlaylistForm.jsx
+++ src/components/PlaylistForm/PlaylistForm.jsx
@@ -3,26 +3,28 @@ import "../PlaylistForm/PlaylistForm.css";
 const PlaylistForm = () => {
   return (
     <div className="playlist-form">
-      <div className="authentication-page">
-        <article className="form-container login-form">
-          <div className="authentication-form-container">
-            <form className="form" action="">
-              <h3 className="h3">Create New Playlist</h3>
-              <label htmlFor="playlist" className="input-label ">
-                Playlist Name
-              </label>
-              <div className="input">
-                <input
-                  className="input"
-                  type="text"
-                  id="playlist"
-                  name="playlist"
-                  placeholder="My Playlist"
-                />
-              </div>
-              <button className="btn btn-secondary">Create Playlist</button>
-            </form>
-          </div>
+      <div className="modal-wrapper">
+        <article className="form-container modal">
+          <button className="modal-close-icon">
+            <span className="material-icons">close</span>
+          </button>
+          <form className="form" action="">
+            <h3 className="h3">Create New Playlist</h3>
+            <label htmlFor="playlist" className="input-label ">
+              Playlist Name
+            </label>
+            <div className="input">
+              <input
+                className="input"
+                type="text"
+                id="playlist"
+                name="playlist"
+                placeholder="My Playlist"
+              />
+            </div>
+
+            <button className="btn btn-secondary">Create Playlist</button>
+          </form>
         </article>
       </div>
     </div>
--- src/pages/PlaylistListingPage/PlaylistLisingPage.jsx
+++ src/pages/PlaylistListingPage/PlaylistLisingPage.jsx
@@ -2,12 +2,18 @@ import "../PlaylistListingPage/PlaylistListingPage.css";
 import { PlaylistCard } from "../../components";
 
 const PlaylistListingPage = () => {
+  const openModalToCreateNewPlaylist = () => {};
   return (
     <div className="main-content-page">
       <div className="menu-bar">
         <div className="page-title h3 text-bold">My Playlists</div>
         <div>
-          <button className="btn btn-cta">Create New Playlist</button>
+          <button
+            className="btn btn-cta"
+            onClick={openModalToCreateNewPlaylist}
+          >
+            Create New Playlist
+          </button>
         </div>
       </div>
       <div className="history-video-container">
