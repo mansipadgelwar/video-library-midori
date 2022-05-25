e1e3269d9b24456dc40fe1e7116edb59b72bb49f
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-25 13:12:10 +0530

    feat: add delete video from like service

---
+++ src/services/like-services/removeVideoFromLikedVideo.jsx
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const removeVideoFromLikedVideos = (authToken,videoId) => {
+    return axios.delete(`/api/user/likes/${videoId}`,{
+        headers: {authorization: authToken}
+    });
+};
+
+export {removeVideoFromLikedVideos};
+
