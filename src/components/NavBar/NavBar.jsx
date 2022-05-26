3cb37bf3e7f501fe01a07206adbce939cdc81f96
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-26 13:52:15 +0530

    Revert "feat: add link to search input"
    This reverts commit 3be6d30c79f3d1e173662d3bf25ff0f58e036b75.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import "../NavBar/NavBar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/authContext/authenticationContext";
 import { useToast } from "../../custom-hooks/useToast";
-import { useData } from "../../context/dataContext/dataContext";
 
 const NavBar = () => {
   const { isAuthorized, authDispatch } = useAuth();
@@ -25,14 +24,12 @@ const NavBar = () => {
           </Link>
           <div className="input-icon icons-left">
             <span className="material-icons">search</span>
-            <Link to="/videolist">
             <input
               className="input"
               type="text"
               id="search-bar"
               name="search-bar"
             />
-            </Link>
           </div>
           {isAuthorized ? (
             <div className="sub-menu">
