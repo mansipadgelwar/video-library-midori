3aa65285843b77f1713c0bfe56fba61665b8d8f9
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-05-26 08:03:15 +0530

    feat: toggle like/dislike from single video page

--- src/components/VideoPage/VideoPanel/VideoPanel.jsx
+++ src/components/VideoPage/VideoPanel/VideoPanel.jsx
@@ -4,19 +4,23 @@ import { useState } from "react/cjs/react.production.min";
 
 const VideoPanel = ({ video }) => {
   const { _id: id, title } = video;  
-  const { addVideoToWatchLater, handleLikedVideos } = useServices();
+  const { addVideoToWatchLater, handleLikedVideos, state } = useServices();
+
+  const findCurrentVideo = state.likes.find((item) => item.id === id);
+  const isVideoExistsInLiked = findCurrentVideo === undefined ? false : true;
 
   return (
     <div className="video-panel">
       <div className="video-sub-menus">
         <button className="btn btn-icon" onClick={() => handleLikedVideos({id,title})}>
-          <span className="material-icons-outlined">thumb_up</span>
+          <span className={`${isVideoExistsInLiked ? "material-icons" : "material-icons-outlined"}`}>thumb_up</span>
+           {isVideoExistsInLiked ? "Liked" : "Like"}         
         </button>
         <button
           className="btn btn-icon"
           onClick={() => addVideoToWatchLater({ id, title })}
         >
-          <span className="material-icons-outlined">favorite_border</span>
+          <span className="material-icons-outlined">watch_later</span>
           Add to Watch Later
         </button>
         <button className="btn btn-icon">
