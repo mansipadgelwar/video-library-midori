1035f71304744772d6c04b4393effcb180550c9e
Mansi Padgelwar <mpadgelwar@gmail.com>
2022-08-08 18:21:58 +0530

    add empty data messages

--- src/pages/WatchLaterPage/WatchLaterPage.jsx
+++ src/pages/WatchLaterPage/WatchLaterPage.jsx
@@ -13,16 +13,22 @@ const WatchLaterPage = () => {
           <div className="page-title h3 text-bold">Watch Later</div>
         </div>
         <div className="history-video-container">
-          {state.watchlater.map((element) => {
-            return (
-              <VideoCard
-                key={element.id}
-                id={element.id}
-                title={element.title}
-                location={"watchLater"}
-              />
-            );
-          })}
+          {state.watchlater.length > 0 ? (
+            state.watchlater.map((element) => {
+              return (
+                <VideoCard
+                  key={element.id}
+                  id={element.id}
+                  title={element.title}
+                  location={"watchLater"}
+                />
+              );
+            })
+          ) : (
+            <div className="logout-content">
+              <h2 className="h2">No videos added to watch later</h2>
+            </div>
+          )}
         </div>
       </div>
     </div>
